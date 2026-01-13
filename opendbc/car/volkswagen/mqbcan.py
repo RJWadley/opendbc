8c03c44977921b314aad7a0003791a3ae374012c
Robbie <robbiethewadley@gmail.com>
2026-01-13 00:07:53 -0700

    adjust stopping signals

--- opendbc/car/volkswagen/mqbcan.py
+++ opendbc/car/volkswagen/mqbcan.py
@@ -136,13 +136,13 @@ def create_acc_accel_control(packer, bus, acc_type, acc_enabled, accel, acc_cont
     acc_hold_type = 4  # hold release / startup
   elif esp_hold:
     acc_hold_type = 3  # hold standby
-  elif stopping:
+  elif stopping_7:
     acc_hold_type = 1  # hold request
   else:
     acc_hold_type = 0
 
   acc_07_values = {
-    "ACC_Anhalteweg": 0.3 if stopping else 20.46,  # Distance to stop (stopping coordinator handles terminal roll-out)
+    "ACC_Anhalteweg": 0.3 if stopping_7 else 20.46,  # Distance to stop (stopping coordinator handles terminal roll-out)
     "ACC_Freilauf_Info": 2 if acc_enabled else 0,
     "ACC_Folgebeschl": 3.02,  # Not using secondary controller accel unless and until we understand its impact
     "ACC_Sollbeschleunigung_02": accel_67 if acc_enabled else 3.01,
