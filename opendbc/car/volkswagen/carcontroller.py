219fa8300db0b2e74f7d4ccd104e77f6e3b9e628
Robbie <robbiethewadley@gmail.com>
2026-01-20 19:46:10 -0700

    revert

--- opendbc/car/volkswagen/carcontroller.py
+++ opendbc/car/volkswagen/carcontroller.py
@@ -120,15 +120,9 @@ class CarController(CarControllerBase):
             acc07_starting_override = False
           elif CS.out.standstill:
             acc07_stopping_override = False
-            acc07_starting_override = True
-            # uphill: hold position to prevent rollback; downhill/flat: gentle negative
-            pitch = CC.orientationNED[1] if len(CC.orientationNED) == 3 else 0
-            if accel > 0:
-              pass  # use requested accel
-            elif pitch > np.radians(1):  # uphill
-              accel = 0
-            else:  # downhill or flat
-              accel = -1
+            acc07_starting_override = False
+            if (accel < 0):
+              accel = self.CCP.ACCEL_MIN
 
         can_sends.extend(self.CCS.create_acc_accel_control(self.packer_pt, self.CAN.pt, CS.acc_type, long_active, accel,
                                                             acc_control, stopping, starting, CS.esp_hold_confirmation,
