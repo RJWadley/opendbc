db6cdec89509a97ea0870c403c63bab4665d1769
Robbie <robbiethewadley@gmail.com>
2026-03-13 09:54:54 -0600

    stop tuning

--- opendbc/car/volkswagen/carcontroller.py
+++ opendbc/car/volkswagen/carcontroller.py
@@ -180,8 +180,8 @@ class CarController(CarControllerBase):
       if self.frame % self.CCP.ACC_CONTROL_STEP == 0:
         long_active = CC.longActive
         accel = float(np.clip(actuators.accel, self.CCP.ACCEL_MIN, self.CCP.ACCEL_MAX) if long_active else 0)
-        stopping = actuators.longControlState == LongCtrlState.stopping
-        starting = actuators.longControlState == LongCtrlState.pid and (CS.esp_hold_confirmation or CS.out.vEgo < self.CP.vEgoStopping)
+        stopping = CS.out.vEgo < self.CP.vEgoStopping and CS.out.standstill
+        starting = CS.out.vEgo < self.CP.vEgoStopping and not CS.out.standstill
 
         # distance button debug helper, force stop or start when distance button is pressed
         if self.CCS == mqbcan and CS.distance_button_pressed:
