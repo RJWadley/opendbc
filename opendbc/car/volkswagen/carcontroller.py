48ace6105d8bc86f01dfc082fd55efcdfdea597d
Robbie <robbiethewadley@gmail.com>
2026-04-06 21:56:16 -0600

    VW: use standstill-manager long_active for acc_control_value too

--- opendbc/car/volkswagen/carcontroller.py
+++ opendbc/car/volkswagen/carcontroller.py
@@ -224,7 +224,7 @@ class CarController(CarControllerBase):
           long_active, accel, stopping, starting, esp_override = \
             self.standstill_manager.update(CS, long_active, accel, stopping, starting, actuators.maxPlannedSpeed)
 
-        acc_control = self.CCS.acc_control_value(CS.out.cruiseState.available, CS.out.accFaulted, CC.longActive, CC.cruiseControl.override)
+        acc_control = self.CCS.acc_control_value(CS.out.cruiseState.available, CS.out.accFaulted, long_active, CC.cruiseControl.override)
         accel = float(np.clip(accel, self.CCP.ACCEL_MIN, self.CCP.ACCEL_MAX) if long_active else 0)
 
         can_sends.extend(self.CCS.create_acc_accel_control(self.packer_pt, self.CAN.pt, CS.acc_type, long_active or CC.cruiseControl.override, accel,
